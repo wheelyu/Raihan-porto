3b097e668ac2aff7a64dd50d0abb2fe476ecfa4b
wheelyu <harun.abdulkarim17@gmail.com>
2024-11-12 23:32:52 +0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-slate-100 text-green-800 py-5 relative z-20">
       <div className="container mx-auto text-center my-auto">
         <p className="mb-4">
-          © {new Date().getFullYear()} Mutiara Pandiangan. All rights reserved.
+          © {new Date().getFullYear()} M Raihan. All rights reserved.
         </p>
       </div>
     </footer>
